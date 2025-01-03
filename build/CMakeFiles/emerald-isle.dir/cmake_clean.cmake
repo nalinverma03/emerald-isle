bdc1068ae816bea90e8865d62af8301d98aedb68
Nalin Verma <88562136+nalinverma03@users.noreply.github.com>
2025-01-03 14:45:55 +0400

    Initial Setup

---
+++ build/CMakeFiles/emerald-isle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emerald-isle.dir/emerald-isle/emerald-isle.cpp.o"
+  "CMakeFiles/emerald-isle.dir/emerald-isle/emerald-isle.cpp.o.d"
+  "CMakeFiles/emerald-isle.dir/emerald-isle/render/shader.cpp.o"
+  "CMakeFiles/emerald-isle.dir/emerald-isle/render/shader.cpp.o.d"
+  "emerald-isle"
+  "emerald-isle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emerald-isle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/emerald-isle.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for emerald-isle.
+# This may be replaced when dependencies are built.
